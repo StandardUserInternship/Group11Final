d8fcb3878dfbaba280f344220e5222a028c89497
Philip <p.gyan@standarduserllc.com>
2022-07-19 14:12:08 -0500

    first push

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
 
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 #initialize app
 
 app = Flask(__name__, instance_relative_config=True)
@@ -7,5 +10,5 @@ app = Flask(__name__, instance_relative_config=True)
 from app import views
 
 #load the config file
-
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 #app.config.from_object('config')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from app import app
 
+user = {'username': 'Philip'}
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Philip'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
