88c2f0924befd901268057f73b06cdb2ca1faa3f
Anthony <Cruzag2002@gmail.com>
2022-07-20 10:10:43 -0500

    debugging

--- app/views.py
+++ app/views.py
@@ -3,8 +3,13 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+@app.route('/login')
 
 
 def index():  # sourcery skip: remove-unreachable-code
     return "<h1>Hello, World!</h1>"
+
+
+def login():
+    return "<h1>Hello welcome to login</h1>"
 
