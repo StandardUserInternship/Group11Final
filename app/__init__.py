9ab010c55627cdae16d5978323623f00eb9fbe4c
Anthony <Cruzag2002@gmail.com>
2022-07-20 15:45:56 -0500

    login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+from config import Config
 
 #initialize app
 
 app = Flask(__name__, instance_relative_config=True)
-
+app.config.from_object(Config)
 
 from app import views
 
--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import os
 
-
-DEBUG = True
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
