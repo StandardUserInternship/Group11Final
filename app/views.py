0f1796258146977ec21e0d842f242acc49e76d22
Anthony <Cruzag2002@gmail.com>
2022-07-20 10:06:54 -0500

    debugging

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from app import app
 
-@app.views('/')
-@app.views('/index')
-def index():
+
+@app.route('/')
+@app.route('/index')
+
+
+def index():  # sourcery skip: remove-unreachable-code
     return "<h1>Hello, World!</h1>"
+
