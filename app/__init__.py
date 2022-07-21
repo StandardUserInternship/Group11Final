3aa21995c07149af5d8df5b6b1cd58ade12496c8
Anthony <Cruzag2002@gmail.com>
2022-07-20 20:17:46 -0500

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #initialize app
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import views
 
