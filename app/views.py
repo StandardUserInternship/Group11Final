394ed72fd3b8e79611c0f8d93072cfa2d655ed82
Anthony <Cruzag2002@gmail.com>
2022-07-20 19:35:39 -0500

    minor changes

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Anthony'}
     posts = [
         {
             'author': {'username': 'John'},
